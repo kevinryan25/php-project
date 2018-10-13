a370aae25fd89054ed54e07239d5b3c306f971f9
BuildTools <ryanrasoarahona@gmail.com>
2018-10-13 16:35:31 +0300

    add public html to github project

---
+++ public_html/ui/main.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Projet php eni 1</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <link rel="stylesheet" href="/ui/css/main.css" />
+    <link rel="stylesheet" href="/ui/css/payslip.css" />
+    <link rel="stylesheet" href="/ui/css/normalize.css" />
+    <link rel="stylesheet" href="/ui/fontawesome/css/fontawesome-all.min.css" />
+    <script src="/ui/scripts/jquery-3.2.1.slim.min.js"></script>
+    <script src="/ui/scripts/ui.js"></script>
+</head>
+
+<body class="background-light">
+    <?php include "ui/modules/main-menu.php"; ?>
+    <main class="background-light vertical-gradient">
+        <h1>Professeur</h1>
+        <section>
+            <div id="edition-form">
+                <form>
+                    <div><label for="year">Année :</label><input class="control" id="year" placeholder="L'année en cours" /></div>
+                    <div><label for="year">N° matricule :</label><input class="control" id="year" placeholder="N° matricule du professeur" /></div>
+                    <div><label for="year">Nom : </label><input class="control" id="year" placeholder="Nom du professeur" /></div>
+                </form>
+            </div>
+            <table id="payslip">
+                <thead>
+                    <tr>
+                        <td>Designation</td>
+                        <td>Nombre d'heures</td>
+                        <td>Montant</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> Langage C</td>
+                        <td> 5</td>
+                        <td> 150€</td>
+                    </tr>
+                    <tr>
+                        <td> Algorithme et structure de donnée</td>
+                        <td> 10</td>
+                        <td> 200€</td>
+                    </tr>
+                    <tr>
+                        <td> Mérise</td>
+                        <td> 2</td>
+                        <td> 30€</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="2">Total</td>
+                        <td> 380€</td>
+                    </tr>
+                </tfoot>
+            </table>
+            <div id="export">
+                <button class="btn btn-secondary">
+                    <i class="fas fa-file-pdf icon"></i><span>Télecharger
+                        pdf</span></button><button class="btn btn-secondary"><i class="fas fa-file-excel icon"></i><span>Télecharger
+                        pdf</span></button></div>
+        </section>
+    </main>
+</body>
+
+</html>
