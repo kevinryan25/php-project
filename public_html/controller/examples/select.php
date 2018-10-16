dbeaa91ad89b6e6325ac1fdc8a22cd7b4f9f1366
kevinryan25 <ryanrasoarahona@gmail.com>
2018-10-16 15:10:07 +0000

    Add the first PHP example

---
+++ public_html/controller/examples/select.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    Sélection avec une requête SQL
+*/
+
+require_once "../includes/db.php"; // Inclusion des fonctions utilitaires
+
+$data = $DB->getData("SELECT * FROM subject"); // Sélection des données
+
+/* Affichage des données */
+foreach($data as $el){ // Boucle foreach(array as element)
+    // utf8_encode : afficher les caractères utilisant l'encodage UTF8
+    var_dump(utf8_encode($el['name'])); // Affichage des informations sur une variable (type valeur);
+    echo "<br/>";// Saut de ligne
+}
